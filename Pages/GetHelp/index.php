8b428bb9d16d1e236b1684d3f3e91702e7bbd41f
Chati <30977493+the-crazy-programmer-chatti@users.noreply.github.com>
2018-10-09 12:27:57 +0530

    Add files via upload

--- Pages/GetHelp/index.php
+++ Pages/GetHelp/index.php
@@ -1,1 +1,23 @@
-
+<html>
+    <head>
+        <title>Alleviate Foundation</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css"/>        
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+        <script type="text/javascript" src="script.js"></script>                 
+    </head>
+    <body>
+        <div id="header">
+                <p class="logo"><img src="../../images/logo.png" height="180" width="220"></p>
+                <div id = "header-right">
+                    <a href="../../index.html">Welcome</a>
+                    <a href="../About/index.html">About</a>
+                    <a href="../GetHelp/index.php" class="active">Get Help</a>
+                    <a href="../Donate/index.html">Donate</a>                
+                </div>
+        </div>
+    </body>
+</html>
