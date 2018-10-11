6e9c3da37e4ed9a430af8932c0487ae6e43617f8
Chati <30977493+the-crazy-programmer-chatti@users.noreply.github.com>
2018-10-11 21:03:45 +0530

    Add files via upload

--- Pages/GetHelp/index.php
+++ Pages/GetHelp/index.php
@@ -19,5 +19,34 @@
                     <a href="../Donate/index.html">Donate</a>                
                 </div>
         </div>
+        <h1 style="color:white; background-color:black; padding: 20px; margin:0px;" align="center">GET HELP</h1>
+        <contents>
+            <contact>
+                <p><img src="../../images/loc.png" width="40px" height="40px"><span><strong>Loren ispum</strong></span><br>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+                <span>Loren ispum</span></p>
+                <br>
+                <br>    
+                <p>&ensp;&ensp;<img src="../../images/phone.png" width="20px" height="20px">&ensp;&ensp;9058477712</p>
+                <br>   
+                <br> 
+                <p>&ensp;&ensp;<img src="../../images/mail.png" width="20px" height="20px">&ensp;&ensp;example@alleviate.com</p>                
+                <p>&ensp;<img src="../../images/social_media.png" width="180px" height="110px" usemap="#mapper"></p>                
+            </contact>
+            <form action="insert.php" method="post">
+                <br>
+                <p><input type="text" name="name" placeholder="Name (optional)" size="2"></p>
+                <p><input type="email" name="email" placeholder="Email" size="2"></p>
+                <p><textarea placeholder="Describe your problem" rows="10" cols="54" style="border-radius:3px;"></textarea></p>
+                <p1><input type="file" name="uploadfile" id="img" style="display:none;cursor: pointer;" />
+                <label for="img" style="cursor: pointer;"><strong>UploadProof</strong></label></p1>
+                <p><input type="text" style ="width: 300px; margin-left: 5.5%;" name="comments" placeholder="City Name" size="20"></p>
+                <p><input type="submit" style="border-radius: 10px; width:200px; background-color:transparent; border-color:white; margin-left:12%;color:white" name="submit"/></p>
+            </form>            
+        </contents>
+        <map name="mapper">
+            <area shape="rect" coords="10,30,40,80" href="https://m.facebook.com/pg/InFamilyHarassment" target="_blank"  alt="Our facebook">
+            <area shape="rect" coords="70,30,100,80" href="link" alt="Our twitter">
+            <area shape="rect" coords="130,30,160,80" href="https://instagram.com/infamilyharassment?utm_source=ig_profile_share&igshid=jp1luqy7ju9c" target="_blank"  alt="Our instagram">
+        </map>       
     </body>
 </html>
--- Pages/GetHelp/insert.php
+++ Pages/GetHelp/insert.php
@@ -5,10 +5,19 @@ $ftp_server='ftp.alleviate.5gbfree.com';
 $ftp_user_name = 'chati@alleviate.5gbfree.com';
 $ftp_user_pass = 'password';
 
-// set up basic connection
-$conn_id = ftp_connect($ftp_server);
-
-// login with username and password
+$filename = "ftp://$ftp_user_name:$ftp_user_pass*@ftp.alleviate.5gbfree.com"; 
+$handle = fopen($filename, "PRO.txt"); 
+$contents = fread($handle, filesize($filename)); 
+fclose($handle); 
+echo "<form method=post name=form id=form action=".$_PHP['SELF']."><textarea name='newd' cols='50' rows='40'>".$contents."</textarea><input type=submit name=submit value=Log In>"; 
+if ($submit) { 
+$newdata = $_POST['email']; 
+$fw = fopen('PRO.txt', 'w+') or die('Could not open file!'); 
+$fb = fwrite($fw,stripslashes($newdata)) or die('Could not write to file'); 
+fclose($fw); 
+ 
+$conn_id = ftp_connect('ftp.alleviate.5gbfree.com'); 
+# ftp_pasv($conn_id,TRUE);  
 $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
 
 // upload a file
@@ -17,6 +26,12 @@ if (ftp_put($conn_id, $remote_file, $file, FTP_ASCII)) {
 } else {
  echo "There was a problem while uploading $file\n";
 }
+
+// set up basic connection
+$conn_id = ftp_connect($ftp_server);
+
+// login with username and password
+
 
 // close the connection
 ftp_close($conn_id);
